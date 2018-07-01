920d125fba2a9b89f386a6c75204965ea1638c8c
yinm <yinm.cminor9@gmail.com>
2018-07-02 08:30:44 +0900

    Check tuple

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,7 +1,7 @@
-enum Fruit {
-    case Grape
-    case Apple
+func returnAnimals() -> (animal1: String, animal2: String) {
+    return("Giraffa", "Elephant")
 }
 
-Fruit.Grape
-Fruit.Apple
+let animals = returnAnimals()
+let animal1 = animals.animal1
+let animal2 = animals.animal2
