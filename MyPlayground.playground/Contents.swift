5718fe5aac1069c250f8e5ff1b34c4356262eca2
yinm <yinm.cminor9@gmail.com>
2018-07-02 08:33:09 +0900

    Check for ~ in

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,7 +1,3 @@
-func returnAnimals() -> (animal1: String, animal2: String) {
-    return("Giraffa", "Elephant")
+for i in 0...9 {
+    print("i: \(i)")
 }
-
-let animals = returnAnimals()
-let animal1 = animals.animal1
-let animal2 = animals.animal2
