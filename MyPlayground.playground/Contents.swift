8d5638e28f939667b35202d759bf1c71c66341c0
yinm <yinm.cminor9@gmail.com>
2018-07-01 16:13:49 +0900

    Check do ~ try ~ catch

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,19 @@
-func showNumber(_ num: Int) -> Int {
-    return num
+enum OriginalError: Error {
+    case InvalidValue
 }
 
-showNumber(10)
+func tripleUp(value: Int) throws -> Int {
+    if value < 0 {
+        throw OriginalError.InvalidValue
+    }
+    
+    return value * 3
+}
+
+do {
+    let result = try tripleUp(value: 20)
+    print(result)
+    try tripleUp(value: -10)
+} catch OriginalError.InvalidValue {
+    print("あかんやつや！")
+}
