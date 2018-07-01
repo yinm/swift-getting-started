f3c08a372ea578a1371cb889da6191f9f78bd099
yinm <yinm.cminor9@gmail.com>
2018-07-01 16:36:43 +0900

    Check instantiate

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,19 +1,3 @@
-enum OriginalError: Error {
-    case InvalidValue
-}
+import UIKit
 
-func tripleUp(value: Int) throws -> Int {
-    if value < 0 {
-        throw OriginalError.InvalidValue
-    }
-    
-    return value * 3
-}
-
-do {
-    let result = try tripleUp(value: 20)
-    print(result)
-    try tripleUp(value: -10)
-} catch OriginalError.InvalidValue {
-    print("あかんやつや！")
-}
+var radio = UISwitch()
