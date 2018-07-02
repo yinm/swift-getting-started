910f04afeee9bc096e849b931959ac42598de493
yinm <yinm.cminor9@gmail.com>
2018-07-02 09:05:16 +0900

    Check $0

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,9 +1,4 @@
 var values = [11, 4, 25, 16, 5]
-let result = values.filter { (x:Int) -> Bool in
-    if x < 10 {
-        return true
-    }
-    return false
-}
+let result = values.filter{$0 < 10}
 
 print(result)
