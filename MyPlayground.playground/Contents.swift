65c815b023145874673669c05d68d24769cb6983
yinm <yinm.cminor9@gmail.com>
2018-07-02 09:09:21 +0900

    Check reduce

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,4 +1,6 @@
 var values = [11, 4, 25, 16, 5]
-let result = values.map{$0 * 2}
+let sum = values.reduce(0) { (nowSum:Int, value:Int) -> Int in
+    return nowSum + value
+}
 
-print(result)
+print(sum)
